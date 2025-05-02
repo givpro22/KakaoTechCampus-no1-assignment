f3c04ec3cad19d4ea7fd3a128501ac86e9c71e25
Young Seo <givpro22@daum.net>
2025-05-03 02:39:36 +0900

    필수기능1,2,3 완료

---
+++ script.js
@@ -0,0 +1,38 @@
+const API_KEY = 'api키 자리';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
+
+async function fetchPopularMovies() {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR`);
+  const data = await response.json();
+  console.log(data.results);
+  displayMovies(data.results); 
+}
+
+fetchPopularMovies();
+
+function displayMovies(movies) {
+  const container = document.getElementById('movieContainer');
+
+  movies.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('movie-card');
+    div.innerHTML = `
+      <img src="${IMAGE_BASE + movie.poster_path}" alt="${movie.title}" />
+      <h3>${movie.title}</h3>
+      <p>${movie.vote_average}</p>
+    `;
+    container.appendChild(div);
+  });
+}
+
+document.getElementById('searchButton').addEventListener('click', () => {
+  const query = document.getElementById('searchInput').value;
+  searchMovies(query);
+});
+
+async function searchMovies(query) {
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&language=ko-KR&query=${encodeURIComponent(query)}`);
+  const data = await response.json();
+  displayMovies(data.results);
+}
